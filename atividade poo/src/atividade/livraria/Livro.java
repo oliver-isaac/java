19a10463a503554920c519da6875485339598f0b
oliver-isaac <isaacoliveiraoliveira67@gmail.com>
2026-03-26 20:08:20 -0300

    Add files via upload

---
+++ atividade poo/src/atividade/livraria/Livro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade.livraria;
+
+/**
+ *
+ * @author 023.970610
+ */
+public class Livro {
+    public String isbn;
+    public String titulo;
+    public String autor;
+    public String obterdetalhes;
+    
+}
